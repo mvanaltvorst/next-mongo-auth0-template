dd761e248a3791a3d6529739008e3d3bf7289ca8
Maurits van Altvorst <mvanaltvorst@icloud.com>
2021-03-26 21:49:13 +0100

    Add auth, start designing home layout

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,56 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+  
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>{error.message}</div>
+  }
+
   return (
-    <div className={styles.container}>
+    <div className="space-y-20 sm:space-y-32 md:space-y-40 lg:space-y-44 overflow-hidden">
       <Head>
-        <title>Create Next App</title>
+        <title>Template App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <header className="relative z-10 max-w-screen-lg xl:max-w-screen-xl mx-auto">
+        <div className="px-4 sm:px-6 md:px-8 mb-14 sm:mb-20 xl:mb-8">
+          Asdf
         </div>
-      </main>
+      </header>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <footer className="bg-gray-50 pt-16 pb-12 sm:pt-20 md:pt-24 xl:pt-32 sm:pb-20 ">
+        <div className="max-w-screen-lg xl:max-w-screen-xl mx-auto divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
+          <ul className="text-sm font-medium pb-14 sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
+            <li className="space-y-5">
+              <h2 className="text-xs font-semibold tracking-wide text-gray-900 uppercase">
+                Contact
+              </h2>
+              <ul className="space-y-4">
+                <li>
+                  <a className="hover:text-gray-900 transition-colors duration-200" href="">
+                    Asdf
+                   </a>
+                </li>
+              </ul>
+            </li>
+            <li className="space-y-5">
+
+            </li>
+          </ul>
+          <div className="pt-10 sm:pt-12">
+            <p className="max-w-4xl text-lg sm:text-2xl font-medium sm:leading-10 space-y-6 mb-6">
+              © Maurits van Altvorst
+            </p>
+          </div>
+        </div>
       </footer>
     </div>
   )
