a1e3deadf8f0b3d982d2526668ed329d26bbd18c
Maurits van Altvorst <mvanaltvorst@icloud.com>
2021-03-26 22:59:37 +0100

    Further develop template, create footer

---
+++ components/Footer.js
@@ -0,0 +1,31 @@
+export default function Footer() {
+    return (
+      <footer className="bg-gray-50 pt-16 pb-12 sm:pt-20 md:pt-24 xl:pt-32 sm:pb-20 ">
+        <div className="max-w-screen-lg xl:max-w-screen-xl mx-auto divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
+          <ul className="text-sm font-medium pb-14 sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
+            <li className="space-y-5">
+              <h2 className="text-xs font-semibold tracking-wide text-gray-900 uppercase">
+                Contact
+              </h2>
+              <ul className="space-y-4">
+                <li>
+                  <a className="hover:text-gray-900 transition-colors duration-200" href="">
+                    Asdf
+                   </a>
+                </li>
+              </ul>
+            </li>
+            <li className="space-y-5">
+
+            </li>
+          </ul>
+          <div className="pt-10 sm:pt-12">
+            <a className="hover:text-gray-900 transition-colers duration-200 max-w-4xl text-lg sm:text-2xl font-medium sm:leading-10 space-y-6 mb-6"
+               href="https://mauritsvanaltvorst.com/">
+              © Maurits van Altvorst
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import { useUser } from '@auth0/nextjs-auth0';
+import Footer from '../components/Footer';
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
@@ -19,39 +20,50 @@ export default function Home() {
         <title>Template App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <header className="relative z-10 max-w-screen-lg xl:max-w-screen-xl mx-auto">
         <div className="px-4 sm:px-6 md:px-8 mb-14 sm:mb-20 xl:mb-8">
-          Asdf
-        </div>
-      </header>
-
-      <footer className="bg-gray-50 pt-16 pb-12 sm:pt-20 md:pt-24 xl:pt-32 sm:pb-20 ">
-        <div className="max-w-screen-lg xl:max-w-screen-xl mx-auto divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
-          <ul className="text-sm font-medium pb-14 sm:pb-20 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10">
-            <li className="space-y-5">
-              <h2 className="text-xs font-semibold tracking-wide text-gray-900 uppercase">
-                Contact
-              </h2>
-              <ul className="space-y-4">
-                <li>
-                  <a className="hover:text-gray-900 transition-colors duration-200" href="">
-                    Asdf
-                   </a>
-                </li>
-              </ul>
-            </li>
-            <li className="space-y-5">
-
-            </li>
-          </ul>
-          <div className="pt-10 sm:pt-12">
-            <p className="max-w-4xl text-lg sm:text-2xl font-medium sm:leading-10 space-y-6 mb-6">
-              © Maurits van Altvorst
-            </p>
+          <div className="border-b border-gray-200 py-4 flex items-center justify-between mb-16 sm:mb-20 -mx-4 px-4 sm:mx-0 sm:px-0">
+            {/* Logo */}
+            <div className="group font-medium flex items-center space-x-3 sm:space-x-4 hover:text-gray-600 transition-colors duration-200 w-full py-2">
+              Template app
+            </div>
+            <div className="flex items-center space-x-6 sm:space-x-10 ml-6 sm:ml-10">
+              <a className="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2" href="/api/auth/login">
+                <span className="sm:hidden">Log in</span>
+                <span className="hidden sm:inline">Log in here</span>
+              </a>
+            </div>
+          </div>
+          <h1 className="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-900 mt-10 mb-8 sm:mt-14 sm:mb-10">
+            Template application.
+          </h1>
+          <p className="max-w-screen-lg text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">
+            A template application.
+          </p>
+          <div className="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4 text-center">
+            <a className="w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200" 
+               href="/api/auth/login">
+              Sign up
+            </a>
+            <a className="w-full sm:w-auto flex-none text-lg leading-6 font-semibold py-3 hover:text-gray-600 transition-colors duration-200"
+               href="/api/auth/login">
+              or log in.
+            </a>
           </div>
         </div>
-      </footer>
+      </header>
+      <section>
+        <div className="px-4 sm:px-6 md:px-8 mb-10 sm:mb-16 md:mb-20">
+          {user ? user.name : "not logged in"} <br/>
+          {user.email}<br/>
+          {user.email_verified}<br/>
+          {user.nickname}<br/>
+          {user.picture}<br/>
+          {user.sub}<br/>
+          {user.updated_at}<br/>
+        </div>
+      </section>
+      <Footer />
     </div>
   )
 }
